e7b34b8985546720189d6b31c541f68bc5273a3a
Zulfahmi Andri <zulfahmi.andri@gmail.com>
2013-09-22 15:28:05 +0700

    First comat camit

--- ampon/src/com/parapon/Geer.java
+++ ampon/src/com/parapon/Geer.java
@@ -2,4 +2,19 @@ package com.parapon;
 
 public class Geer {
 
+	public Geer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private String tryme;
+
+	public String getTryme() {
+		return tryme;
+	}
+
+	public void setTryme(String tryme) {
+		this.tryme = tryme;
+	}
+	
+	
 }
